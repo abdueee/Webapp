5380e38dddec79832c2e8a3610764d205d624ad4
Huzaifa Ali <57848119+huzaifa154@users.noreply.github.com>
2021-01-30 21:26:05 -0500

    Update signup_conn.php

--- templates/signup_conn.php
+++ templates/signup_conn.php
@@ -1,12 +1,14 @@
 <?php
 	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isSet($_POST['sentForm'])) {
 		$conn = mysqli_connect('localhost', 'root', 'mohali2000','webapp') or die("Connection failed: " . mysqli_connect_error());
-		if (isSet($_POST['user']) && isSet($_POST['email']) && isSet($_POST['pass'])) {
-			$user = $_POST['user'];
-			$pass = $_POST['pass'];
-			$email = $_POST['email'];
+		if (isSet($_POST['firstName']) && isSet($_POST['lastName']) && isSet($_POST['email']) && isSet($_POST['pass']) && isSet($_POST['dob']) ) {
+			$firstName = $_POST['firstName'];
+			$lastName = $_POST['lastName'];
+            $email = $_POST['email'];
+            $pass = $_POST['pass'];
+            $dob = $_POST['dob'];
  
-			$sql = "INSERT INTO `users` (`username`,`password`,`email`) VALUES ('$user', '$pass', '$email')";
+			$sql = "INSERT INTO `user` (`first name`,`last name`, `email`, `password`, `date of birth`) VALUES ('$firstName', '$lastName', '$email', '$pass', '$dob')";
  
 			$query = mysqli_query($conn,$sql);
 			if ($query) {
